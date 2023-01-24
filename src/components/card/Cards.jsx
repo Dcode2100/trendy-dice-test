42d0e6a2c9da1fee4a099d18de1e15230bf896ef
Dikshant Solanki <dcode2100@gmail.com>
2023-01-24 09:32:02 +0530

    Card component and card List

---
+++ src/components/card/Cards.jsx
@@ -0,0 +1,59 @@
+import React,{useState} from 'react'
+import {Card} from 'antd';
+import {
+  EditOutlined,
+  MailOutlined,
+  PhoneOutlined,
+  GlobalOutlined,
+  HeartOutlined,
+  HeartFilled,
+  DeleteFilled,
+} from "@ant-design/icons";
+
+const Cards = (user) => {
+  const [liked, setLiked] = useState(false);
+
+  return (
+    <div>
+      <Card
+        className="card m-4"
+        cover={<img alt="example" src={user.user.avatar} />}
+        actions={[
+          liked ? (
+            <HeartFilled
+              style={{ color: "red", fontSize: "20px" }}
+              onClick={() => setLiked((prevstate) => !prevstate)}
+            />
+          ) : (
+            <HeartOutlined
+              style={{ color: "red", fontSize: "20px" }}
+              onClick={() => setLiked((prevstate) => !prevstate)}
+            />
+          ),
+          <EditOutlined key="edit" />,
+          <DeleteFilled />,
+        ]}
+      >
+        <div className={""}>
+          <h3 className="flex font-medium">
+            {user.user.name}
+          </h3>
+          <div className="flex flex-row justify-start items-center">
+            <MailOutlined />
+            <p className="ml-3 my-0 mr-0">{user.user.email}</p>
+          </div>
+          <div className="flex flex-row justify-start items-center">
+            <PhoneOutlined />
+            <p className="ml-3 my-0 mr-0">{user.user.phone}</p>
+          </div>
+          <div className="flex flex-row justify-start items-center">
+            <GlobalOutlined />
+            <p className="ml-3 my-0 mr-0">{user.user.website}</p>
+          </div>
+        </div>
+      </Card>
+    </div>
+  );
+}
+
+export default Cards
---
+++ src/components/cardlist/Cardlist.jsx
@@ -0,0 +1,37 @@
+// eslint-disable-next-line
+import { Col,Row } from "antd";
+import React, { useState, useEffect } from "react";
+import Cards from "../card/Cards";    
+
+const Cardlist = () => {
+  // eslint-disable-next-line
+  const [users, setUser] = useState([]);
+  
+  useEffect(() => {
+       fetch("https://jsonplaceholder.typicode.com/users")
+        .then((res) => res.json())
+        .then((data) => setUser(data))
+},[]);
+
+  return (
+    <div>
+      <Row className="row" wrap>
+        {users.map((user) => (
+          <Col
+            className=" ant-col ant-col-xs-24 ant-col-sm-24 ant-col-md-8 ant-col-lg-8 ant-col-xl-6"
+            xs={24}
+            sm={24}
+            md={8}
+            lg={8}
+            xl={6}
+            key={user.id}
+          >
+            <Cards user={user} />
+          </Col>
+        ))}
+      </Row>
+    </div>
+  );
+};
+
+export default Cardlist;
